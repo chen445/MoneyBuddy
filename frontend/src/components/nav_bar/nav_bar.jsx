783418faf44cfb532ddb4d9060c23504eeafd111
Ying <cy240705@gmail.com>
2021-06-15 19:20:24 -0700

    working on nav_bar

--- frontend/src/components/nav_bar/nav_bar.jsx
+++ frontend/src/components/nav_bar/nav_bar.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {ImCalendar} from "react-icons/im";
 import {BsBarChart} from 'react-icons/bs';
 import {AiOutlineShop} from 'react-icons/ai'
-
+import {GiCutDiamond} from 'react-icons/gi'
+import {FaRegUserCircle} from 'react-icons/fa'
 
 class NavBar extends React.Component{
   constructor(props) {
@@ -11,25 +12,33 @@ class NavBar extends React.Component{
 
   render(){
       return (
-        <div>
-          <div>
-            <ul>
-               <li>
-                    
-               </li>
-              <li>
-                <a href=""><ImCalendar/><br />Today</a>
-              </li>
-              <li>
-                  <a href=""><BsBarChart/><br/> Report</a>
-              </li>
-              <li>
-                  <a href=""><AiOutlineShop/><br/>Icon Shop</a>
-              </li>
-              <li></li>
-            </ul>
+        <div
+          className="nav-bar-left"
+          style={{ backgroundImage: `url(${window.homeimg}` }}
+        >
+          <div className="user">
+            <FaRegUserCircle size={45} />
+            <h2>{this.props.currentUser.username}</h2>
           </div>
-          <div></div>
+          <div className="point">
+            <GiCutDiamond color={"lightblue"} size={45} />
+            {this.props.currentUser.point}
+          </div>
+          <a href="">
+            <ImCalendar size={45} />
+            <br />
+            <h2>Today</h2>
+          </a>
+          <a href="">
+            <BsBarChart size={45} />
+            <br />
+            <h2>Report</h2>
+          </a>
+          <a href="">
+            <AiOutlineShop size={45} />
+            <br />
+            <h2>Icon Shop</h2>
+          </a>
         </div>
       );
   }
