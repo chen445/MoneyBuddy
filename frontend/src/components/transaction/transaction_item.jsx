9fef0aed520c553656c2607abedc0f44bf4b1d84
Ying <cy240705@gmail.com>
2021-06-30 00:55:42 -0700

    working on transactions detail

--- frontend/src/components/transaction/transaction_item.jsx
+++ frontend/src/components/transaction/transaction_item.jsx
@@ -6,7 +6,7 @@ import { IconsList } from '../icon/icon_list';
 class TransactionItem extends React.Component {
     constructor(props) {
         super(props)
-        
+        this.state = { showDetail: false};
         this.newDate = this.newDate.bind(this)
 
     }
@@ -16,21 +16,54 @@ class TransactionItem extends React.Component {
     }
 
     render() {
+      let detailButton;
+      if (this.state.showDetail) {
+        detailButton = (
+          <button
+            onClick={(e) => {
+              {
+                this.setState({ showDetail: false });
+              }
+            }}
+          >
+            -
+          </button>
+        );
+      } else {
+        detailButton = (
+          <button
+            onClick={(e) => {
+              {
+                this.setState({ showDetail: true });
+              }
+            }}
+          >
+            +
+          </button>
+        );
+      }
+
+      let transactionsDetail = this.state.showDetail
+        ? this.props.description
+        : "";
+    
         return (
           <div className="detail-transaction">
             <div>
               <ul className="grid-container">
                 <li className="grid-item">{this.newDate(this.props.date)}</li>
                 <li className="grid-item">{this.props.type}</li>
-
-                <li className="grid-item">
+                <li className="grid-item" style={{width:"250px"}}>
                   {IconsList[this.props.icon - 1]
                     ? IconsList[this.props.icon - 1]
-                    : null}{" "}
+                    : null}
                   {this.props.category}
                 </li>
-                {/* <li>{this.props.description}</li> */}
                 <li className="grid-item">${this.props.amount}</li>
+                <li className="grid-item">
+                  {this.props.description === "" ? "" : detailButton}
+                </li>
+                <li>{transactionsDetail}</li>
               </ul>
             </div>
           </div>
--- frontend/src/components/transaction/transactions.jsx
+++ frontend/src/components/transaction/transactions.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import TransactionItem from './transaction_item';
 import { Link } from "react-router-dom";
+import { MdDescription } from 'react-icons/md';
 
 class Transactions extends React.Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class Transactions extends React.Component {
 
     this.state = {
       transactions: [],
+      show: false,
     };
   }
 
@@ -20,26 +22,65 @@ class Transactions extends React.Component {
     this.setState({ transactions: newState.transactions });
   }
 
-
   render() {
-    return (
-      <div className="index-trans">
-        <div className="detail">
-        {/* <div>
-          <Link to="/create_transaction">+</Link>
-        </div> */}
-          <h2>Transactions</h2>
-          {this.state.transactions.map((trans) => (
+    const sortedTransactions = this.state.transactions.sort(
+      (a, b) => new Date(b.date) - new Date(a.date)
+    );
+
+    let transactionsView = this.state.show
+      ? sortedTransactions
+          .slice(8)
+          .map((trans) => (
             <TransactionItem
               key={trans.id}
               category={trans.category}
-              // description={trans.description}
+              description={trans.description}
+              amount={trans.amount}
+              icon={trans.icon}
+              type={trans.type}
+              date={trans.date}
+            />
+          ))
+      : "";
+    let buttonView;
+    if (this.state.show) {
+      buttonView = "";
+    } else {
+      buttonView = (
+        <button
+          onClick={(e) => {
+            {
+              this.setState({ show: true });
+            }
+          }}
+        >
+          + Show more
+        </button>
+      );
+    }
+
+  
+    
+    return (
+      <div className="index-trans">
+        <div className="detail">
+          {/* <div>
+          <Link to="/create_transaction">+</Link>
+        </div> */}
+          <h2>Transactions</h2>
+          {sortedTransactions.slice(0, 7).map((trans) => (
+            <TransactionItem
+              key={trans.id}
+              category={trans.category}
+              description = {trans.description}
               amount={trans.amount}
               icon={trans.icon}
               type={trans.type}
               date={trans.date}
             />
           ))}
+          {transactionsView}
+          {buttonView}
         </div>
       </div>
     );
